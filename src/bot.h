8e3228fc79321178c50b93b9c9af38355ca6c8c2
Christopher Cowden <chris.cowden@gmail.com>
2018-06-12 01:47:41 -0500

    Starting from a clean bot.

---
+++ src/bot.h
@@ -0,0 +1,24 @@
+#ifndef BOT_H
+#define BOT_H
+
+
+///////////////////////////////////
+// C S Cowden  11 June 2018
+// The bot base class for the various sub-agents
+// implemented in the TURK.
+/////////////////////////////////////
+
+namespace Turk {
+
+class bot {
+
+public:
+
+
+
+};
+
+} // end Turk namespace
+
+
+#endif
